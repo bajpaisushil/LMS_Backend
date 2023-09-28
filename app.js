c076a4e1f56e0505babbd548eaa96c89a84bea77
bajpaisushil <sushilbajpai2003@gmail.com>
2023-09-28 15:43:10 +0530

    LMS Server

---
+++ app.js
@@ -0,0 +1,35 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
+import userRoutes from './routes/user.route.js';
+import courseRoutes from './routes/course.route.js';
+import paymentRoutes from  './routes/payment.route.js';
+import { errorMiddleware } from './middlewares/error.middleware.js';
+import morgan from 'morgan';
+import { config } from 'dotenv';
+config();
+
+const app=express();
+app.use(express.json());
+console.log(process.env.FRONTEND_URL);
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true
+}))
+// app.use(cors());
+app.use(morgan('dev'))
+app.use(cookieParser());
+app.use('/ping', (req, res)=>{
+    res.send('Pong');
+})
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/courses', courseRoutes);
+app.use('/api/v1/payments', paymentRoutes);
+
+app.all('*', (req, res)=>{
+    res.status(404).send('OOPS! 404 Page not found');
+})
+app.use(errorMiddleware);
+
+
+export default app;
---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,37 @@
+import AppError from "../utils/appError.js";
+import jwt from 'jsonwebtoken';
+
+
+export const isLoggedIn=(req, res, next)=>{
+    const {token}=req.cookies;
+    if(!token){
+        return next(new AppError('Unauthenticated! Please Login', 401))
+    }
+    const tokenDetails=jwt.verify(token, process.env.JWT_SECRET);
+    if(!tokenDetails){
+        return next(new AppError('Unauthenticated! Please Login', 401))
+    }
+    req.user=tokenDetails;
+    next();
+}
+
+export const authorizedRoles=(...roles)=> (req, res, next)=>{
+    const currentRole=req.user.role;
+    if(!roles.includes(currentRole)){
+        return next(
+            new AppError('You do not have permission to access this route', 403)
+        )
+    }
+    next();
+}
+
+export const authorizedSubscriber=async(req, res, next)=>{
+    const subscriptionStatus=req.user.subscription.status;
+    const currentRole=req.user.role;
+    if(currentRole!=='ADMIN' && subscriptionStatus!=='active'){
+        return next(
+            new AppError('Please subscribe to access this route', 403)
+        )
+    }
+    next();
+}
